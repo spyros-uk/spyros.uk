54792843ec3abf63e77bf9e5a9131d7b8b99b743
Spyros <dev@spyros.uk>
2022-10-29 00:30:56 +0100

    WIP: Sets up aws s3 with terraform

---
+++ terraform/s3.tf
@@ -0,0 +1,57 @@
+
+resource "aws_s3_bucket" "spyros_uk_s3" {
+  bucket = "staging.spyros.uk"
+
+  tags = {
+    Name        = "staging.spyros.uk"
+    Environment = "Dev"
+  }
+
+}
+
+resource "aws_s3_bucket_website_configuration" "spyros_uk_s3_config" {
+  bucket = aws_s3_bucket.spyros_uk_s3.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "spyros_uk_s3_public_access_config" {
+  bucket = aws_s3_bucket.spyros_uk_s3.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "spyros_uk_s3_policy" {
+  bucket = aws_s3_bucket.spyros_uk_s3.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "spyros_uk_s3_policy"
+    Statement = [
+      {
+        Sid       = "AllowPublicRead"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource = [
+          aws_s3_bucket.spyros_uk_s3.arn,
+          "${aws_s3_bucket.spyros_uk_s3.arn}/*",
+        ]
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = "true"
+          }
+        }
+      },
+    ]
+  })
+}
