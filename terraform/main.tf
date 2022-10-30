e22d1a1471ccf6a124f8c2794ab7325eb0df1c8e
Spyros <dev@spyros.uk>
2022-10-30 17:56:58 +0000

    Saves tfstate to an S3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,15 +2,6 @@ variable "environment" {
   type = string
 }
 
-variable "AWS_ACCESS_KEY_ID" {
-  type = string
-}
-
-
-variable "AWS_SECRET_KEY" {
-  type = string
-}
-
 locals {
   config = jsondecode(file("${path.module}/config/config.json"))
 }
@@ -26,9 +17,12 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-provider "aws" {
-  region  = "us-west-2"
-  # profile = "spyros-uk"
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_KEY
+provider "aws" {}
+
+terraform {
+  backend "s3" {
+    bucket = "spyros-uk-tfstate"
+    key    = "tfstate"
+    region = "eu-west-2"
+  }
 }
